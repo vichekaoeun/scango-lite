3d6c0d88ef77c3c409cd9461d5beb8a83af79d01
Vicheka Oeun <oeunvicheka95@gmail.com>
2025-05-20 00:55:37 -0400

    commiting this so i can work on it on my laptop instead

--- rules/secrets.go
+++ rules/secrets.go
@@ -1,1 +1,12 @@
 package main
+
+import(
+	"fmt"
+	"go/ast"
+	"go/token"
+	"strings"
+)
+
+func checkForSecrets(n ast.Node, fset *token.FileSet, filename string) { //takes the current ast node, fset gets line info and filename for context
+	
+}
